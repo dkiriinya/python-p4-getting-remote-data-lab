696ca10ff4c14b534c34fa380b24482d9d1d1296
dkiriinya <dkiriinya06sc@gmail.com>
2024-04-05 11:02:15 +0300

    Practice sending GET requests using Python. Build a reusable class for sending requests and handling responses.

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,17 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        response = requests.get(self.url)
+        return response.content
 
     def load_json(self):
-        pass
+        json_list = []
+        json_data = json.loads(self.get_response_body())
+        
+        for json_ in json_data:
+            json_list.append(json_)
+            
+        return json_list
+    
+get_requester = GetRequester('https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json')
+print(get_requester.load_json())
